a8e1fb6a37a3be1080f589fc4ab6df37bbc8e5ba
Jason Widjaja <jasonwidjaja@icloud.com>
2023-09-13 21:57:08 -0400

    Day 22: Render app titles into columns

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas
 
 st.set_page_config(layout = "wide")
 
@@ -10,7 +11,11 @@ with col1:
 with col2:
     st.title("Jason Widjaja")
     content = """
-    Hi, my name is Jason Widjaja.. (ADD MORE STUFF HERE)
+    Hello, my name is Jason Widjaja and I'm currently a third year Mechanical 
+    Engineering major with a concentration in Robotics. Throughout my tenure at 
+    Northeastern, I've had the pleasure of being part of clubs such as NU SEDS,
+    Electric Racing, and now Generate which have taught me invaluable skills to 
+    becoming a better engineer. 
     """
     st.info(content)
 
@@ -18,3 +23,15 @@ content2 = """
 Below you can find some of the apps I have built in Python. Feel free to contact me!
 """
 st.write(content2)
+
+col3, col4 = st.columns(2)
+
+df = pandas.read_csv("data.csv", sep = ";")
+
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
