a74412d805847e465a9ebec68b4a7adedc8940ae
Jason Widjaja <jasonwidjaja@icloud.com>
2023-09-16 21:16:52 -0400

    Day 23: Add navigation menu and Contact Us page

---
+++ send_email.py
@@ -0,0 +1,12 @@
+import smtplib, ssl
+import os
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+    username = "jasonwidjaja57@gmail.com"
+    password = os.getenv("PASSWORD")
+    receiver = "jasonwidjaja57@gmail.com"
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
